a7f82d7f4f0d5edabedd7a0afeebd0678bb9a472
manishboro <officialmanishboro@gmail.com>
2024-05-25 22:44:39 +0530

    tutorial complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function ListItem({ title, listItemIndex, handleDeleteItem }) {
+  console.log("hey");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="item">
+      {title}
+      <button onClick={() => handleDeleteItem(listItemIndex)}>Delete</button>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  const [item, setItem] = useState("");
+
+  const [items, setItems] = useState([]);
+
+  const handleAddItem = ({ id, title }) => {
+    if (!id || !title) return;
+    setItem("");
+    setItems((items) => [...items, { id, title }]);
+  };
+
+  const handleDeleteItem = (index) => {
+    setItems((items) => {
+      let _items = [...items];
+      _items.splice(index, 1);
+      return _items;
+    });
+  };
+
+  return (
+    <div className="main">
+      <form
+        className="input-container"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddItem({ id: nanoid(), title: item });
+        }}
+      >
+        <input value={item} onChange={(e) => setItem(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+
+      {items.length ? (
+        <div className="items-container">
+          {items.map((item, idx) => (
+            <ListItem
+              key={item.id}
+              title={item.title}
+              listItemIndex={idx}
+              handleDeleteItem={handleDeleteItem}
+            />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
