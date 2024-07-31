daf2b70447320fd5ebf030c3ab2d60033a223612
manishboro <officialmanishboro@gmail.com>
2024-07-31 22:52:49 +0530

    changes complete

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,7 +1,13 @@
 import { use } from "react";
 import CommentCard from "./CommentCard";
+import { AuthContext } from "../context/AuthContext";
 
+// use can only be called inside of a functional component and other hooks.
 const Comments = ({ fetchComments }) => {
+  const auth = use(AuthContext);
+
+  if (!auth.isAdmin) return "Youre not an admin";
+
   const comments = use(fetchComments);
 
   return (
